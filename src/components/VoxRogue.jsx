471f82625b900db2a95fbe0b138815385d723462
Devansh Ruia <ruiadevansh@gmail.com>
2026-03-01 14:31:31 -0800

    fix: add useEffect to VoxRogue imports

--- src/components/VoxRogue.jsx
+++ src/components/VoxRogue.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useGame } from "../hooks/useGame";
 import { useVoice } from "../hooks/useVoice";
 import { GameLog } from "./GameLog";
