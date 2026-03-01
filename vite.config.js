8be9ea282acb55abb4c5812ee547ba2874cad677
Devansh Ruia <ruiadevansh@gmail.com>
2026-02-28 20:56:50 -0800

    FixViteConfig

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@supabase/supabase-js'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@supabase\/supabase-js/, /node_modules/],
+    },
+  },
 })
